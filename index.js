8221ec98f55badd7651f82e7fa3037bdcbc51efb
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2024-10-15 16:05:43 +0300

    v0.3 "add route and controller subject"

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ require("dotenv").config();
 const dbConnect = require("./config/dbConnect");
 const userRoutes = require("./routes/userRoutes");
 const { globalErrHandler, notFound } = require("./middlewares/globaErrHandler");
+const subjectRoutes = require("./routes/subjectRoutes");
 const port = process.env.PORT;
 dbConnect();
 app.use(express.json());
 //routes
 app.use("/api/users", userRoutes);
+app.use("/api/subject", subjectRoutes);
 //Gestion des erreurs
 app.use(notFound);
 app.use(globalErrHandler);
